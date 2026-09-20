b88f3d1e4a66b3440d54b71b88975e59178b4e62
محمد عادل محمد بيومي <85137010+mohamedadel452@users.noreply.github.com>
2026-09-20 15:10:04 +0300

    Q1 is answered

---
+++ C#-Basics05-Assigment/Book.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+    class Book
+    {
+    // this private field cant be used out this class because it is private 
+     private string password = "secret";
+
+
+    }
+
--- C#-Basics05-Assigment/Program.cs
+++ C#-Basics05-Assigment/Program.cs
@@ -8,7 +8,32 @@ namespace Session5Assignment
     {
         static void Main(string[] args)
         {
-           //dev barnch is created 
+
+
+            #region Q1: Private Modifier
+            Console.WriteLine("=== Q1: Private Modifier ===");
+            // Try to print it from Main (outside the class). What happens, and why?
+          
+            // Uncommenting the next line will cause a Compile-Time Error: 
+           
+            // 'Book.password' is inaccessible due to its protection level.
+           
+            
+            Book myBook = new Book();
+            
+            
+            //Console.WriteLine(myBook.password); 
+        
+            // WHY? Because 'private' hides the data from the outside world (Encapsulation).
+            // It protects internal state from being corrupted by external classes.
+            Console.WriteLine("Cannot access myBook.password because it is private. (See comments in code)");
+            Console.WriteLine();
+            #endregion
+
+
+
+
+
         }
     }
 }
